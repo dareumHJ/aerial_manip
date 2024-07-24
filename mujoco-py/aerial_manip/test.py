334dbc4962b648b57e060320714ac4d0ebe353a8
Park Hajun <dareum@kaist.ac.kr>
2024-07-24 17:37:53 +0900

    some update w/ walkon and manip dir

--- mujoco-py/aerial_manip/test.py
+++ mujoco-py/aerial_manip/test.py
@@ -1,7 +1,9 @@
 import numpy as np
+import mujoco_py
 import mujoco
 import mujoco.viewer
 import mediapy as media
+import matplot
 
 xml = "ur3e.xml"
 model = mujoco.MjModel.from_xml_path(xml)
@@ -21,71 +23,87 @@ pi = np.pi
 data.qpos = [0, pi/2, -pi/2] #ENABLE if you want test circle
 
 #Init parameters
+M = np.zeros((model.nv, model.nv))
 jacp = np.zeros((3, model.nv)) #translation jacobian
 jacr = np.zeros((3, model.nv)) #rotational jacobian
-step_size = 10
-tol = 0.01
-alpha = 0.5
-damping = 0.01
+dt = 0.005
+gain = 20
+tol = 0.001
+damping = 0.0
+Kp = 100.0
+Kd = 10.0
+
 
 #Get error.
-end_effector_id = model.body('E_E').id #"End-effector we wish to control.
-current_pose = data.body(end_effector_id).xpos #Current pose
+end_effector_id = model.body('E_E').id
+current_pose = data.body(end_effector_id).xpos
 
-goal = [0.07, -0.05, 0.3] #Desire position
+#Goal Position set
+goal = [-0.13, 0.0, 0.264] #Desire position
+goal2 = [-0.08, 0.05, 0.28]
 
+#Error initialization
 error = np.subtract(goal, current_pose) #Init Error
+prev_error = 0
 
+#Function Definitions
 def check_joint_limits(q):
-    """Check if the joints is under or above its limits"""
     for i in range(len(q)):
         q[i] = max(model.jnt_range[i][0], min(q[i], model.jnt_range[i][1]))
 
-def circle(t: float, r: float, h: float, k: float, f: float) -> np.ndarray:
-    """Return the (x, z) coordinates of a circle with radius r centered at (h, k)
-    as a function of time t and frequency f."""
-    x = r * np.cos(2 * np.pi * f * t) + h
-    y = 0.04
-    z = r * np.sin(2 * np.pi * f * t) + k
-    return np.array([x, y, z])
+def inv_kinematics(model, data, jacp, jacr, goal, end_effector_id, error):
+    #Calculate jacobian
+    mujoco.mj_jac(model, data, jacp, jacr, goal, end_effector_id)
+    n = jacp.shape[1]
+    I = np.identity(n)
+    product = jacp.T @ jacp + damping * I
+    if np.isclose(np.linalg.det(product), 0):
+        j_inv = np.linalg.pinv(product) @ jacp.T
+    else:
+        j_inv = np.linalg.inv(product) @ jacp.T
+    delta_q = j_inv @ error
+    return delta_q
+
+# Get goal qpos using iterative inverse kinematics
+while (np.linalg.norm(error) > tol):
+    dq = inv_kinematics(model, data, jacp, jacr, goal2, end_effector_id, error)
+    q = data.qpos.copy()
+    q += dq * 0.05
+    data.qpos = q
+    mujoco.mj_forward(model, data)
+    error = goal2 - data.body(end_effector_id).xpos
+print(data.body(end_effector_id).xpos)
+print(np.linalg.norm(error))
+q_goal = data.qpos.copy()
+
+# reset
+mujoco.mj_resetData(model, data)
+data.qpos = [0, pi/2, -pi/2]
 
 #Simulate
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while data.time < DURATION:
+        mujoco.mj_fullM(model, M, data.qM)
+        cg = data.qfrc_bias
+
+        # calculate error and error_d
+        error = np.subtract(q_goal, data.qpos)
+        error_d = (error - prev_error)/dt
         
-        goal = circle(data.time, 0.07, 0, 0.3, 3) #ENABLE to test circle.
-        if (np.linalg.norm(error) >= tol):
-            #Calculate jacobian
-            mujoco.mj_jac(model, data, jacp, jacr, goal, end_effector_id)
-            #Calculate delta of joint q
-            n = jacp.shape[1]
-            I = np.identity(n)
-            product = jacp.T @ jacp + damping * I
-
-            if np.isclose(np.linalg.det(product), 0):
-                j_inv = np.linalg.pinv(product) @ jacp.T
-            else:
-                j_inv = np.linalg.inv(product) @ jacp.T
-
-            delta_q = j_inv @ error
-
-            #Compute next step
-            q = data.qpos.copy()
-            q += step_size * delta_q
-            
-            #Check limits
-            # check_joint_limits(data.qpos)
-            
-            #Set control signal
-            data.ctrl = q
-            
-            #Step the simulation.
-            mujoco.mj_step(model, data)
-
-            error = np.subtract(goal, data.body(end_effector_id).xpos)
+        #Check limits
+        # check_joint_limits(data.qpos)
         
-        else:
-            mujoco.mj_step(model, data)
+        ctrlval = M @ (Kp * error + Kd * error_d)
+        ctrkvak = J
+        #Set control signal
+        data.ctrl = cg + ctrlval
+
+        print(np.linalg.norm(error))
+
+        #Step the simulation.
+        mujoco.mj_step(model, data)
+        #Update previous error
+        prev_error = error
 
         #Render and save frames.
         if len(frames) < data.time * FRAMERATE:
@@ -98,7 +116,6 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
             viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTFORCE] = True
             viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = False
 
-            # tweak scales of contact visualization elements
             model.vis.scale.contactwidth = 0.05
             model.vis.scale.contactheight = 0.015
             model.vis.scale.forcewidth = 0.025
--- mujoco-py/walkon/ik_solver_new.py
+++ mujoco-py/walkon/ik_solver_new.py
@@ -5,7 +5,7 @@ import mediapy as media
 import math
 
 # Choose a model
-xml = "WALKON5_left_leg.xml"
+xml = "test_leg2.xml"
 model = mujoco.MjModel.from_xml_path(xml)
 data = mujoco.MjData(model)
 renderer = mujoco.Renderer(model)
@@ -34,24 +34,22 @@ def compute_com(model, data):
 mujoco.mj_resetData(model, data)
 
 # 70 ~ 90
-des_qpos1 = 0*np.pi/180
+des_qpos1 = 80*np.pi/180
 des_qpos2 = 90*np.pi/180
 
 # des_qpos1 = 0*np.pi/180
 # des_qpos2 = 0*np.pi/180
 
-# find err value
-joint1_pos = data.body(model.body('L_FOOT').id).xipos.copy()
-joint2_pos = data.body(model.body('L_ANKLE').id).xipos.copy()
-com = compute_com(model, data)
-unit_vec1 = [0, -1, 0]
-unit_vec2 = [0, 0, 1]
-unit_vec2 = unit_vec2 / np.linalg.norm(unit_vec2)
+unit_vec1 = np.array([1, 0, 0])
+unit_vec2 = np.array([0, 1, 0])
+unit_vec3 = np.array([0, 0, 1])
+norm_vec1 = np.array([1, 0, 1])
+norm_vec1 = norm_vec1 / np.linalg.norm(norm_vec1)
 
-Ks = 30.0
-Bs = 3.0
-Ks2 = 30.0
-Bs2 = 3.0
+Ks = 50.0
+Bs = 4.0
+Ks2 = 50.0
+Bs2 = 4.0
 
 plant_idx = (data.qpos.size) - 1
 inver_idx = (data.qpos.size) - 2
@@ -62,16 +60,26 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
     while data.time < DURATION:
         # mujoco.mj_differentiatePos(model, dq, step_size, qpos0, data.qpos)
 
-        joint1_pos = data.body(model.body('L_FOOT').id).xipos.copy()
-        joint2_pos = data.body(model.body('L_ANKLE').id).xipos.copy()
+        # ROTATION MATRIX (Global -> Local)
+        mat1 = (data.body(model.body('L_FOOT').id).xmat.copy()).reshape(3, 3)
+        mat2 = (data.body(model.body('L_ANKLE').id).xmat.copy()).reshape(3, 3)
+
+
+        joint1_pos = data.body(model.body('L_FOOT').id).xpos.copy() + mat1 @ np.array([-0.15552, 0.17526, -0.9975])
+        joint2_pos = data.body(model.body('L_ANKLE').id).xpos.copy() + mat2 @ np.array([-0.06603, 0.10476, -0.90801])
         com = compute_com(model, data)
         det_vec1 = com - joint1_pos
         det_vec2 = com - joint2_pos
 
-        mat1 = (data.body(model.body('L_FOOT').id).xmat.copy()).reshape(3, 3)
-        mat2 = (data.body(model.body('L_ANKLE').id).xmat.copy()).reshape(3, 3)
-        cur_pos1 = math.acos(np.dot((mat1 @ det_vec1), unit_vec1) / np.linalg.norm(det_vec1))
-        cur_pos2 = math.acos(np.dot((mat2 @ det_vec2), unit_vec2) / np.linalg.norm(det_vec2))
+
+        proj_vec1 = np.linalg.inv(mat1) @ det_vec1
+        proj_vec1 = proj_vec1 - (np.dot(proj_vec1, unit_vec2)) * unit_vec2
+
+        proj_vec2 = np.linalg.inv(mat2) @ det_vec2
+        proj_vec2 = proj_vec2 - (np.dot(proj_vec2, norm_vec1)) * norm_vec1
+        
+        cur_pos1 = math.acos(np.dot((proj_vec1), unit_vec1) / np.linalg.norm(proj_vec1))
+        cur_pos2 = math.acos(np.dot((proj_vec2), unit_vec2) / np.linalg.norm(proj_vec2))
         err1 = -(des_qpos1 - cur_pos1)
         # err1 = des_qpos1 - (data.qpos.copy())[plant_idx]
         err2 = -(des_qpos2 - cur_pos2)
@@ -83,13 +91,13 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         #Set control signal
         ctrlval1 = (Ks * err1 + Bs * errdiff1)
         ctrlval2 = (Ks2 * err2 + Bs2 * errdiff2)
-        data.ctrl = [ctrlval2, ctrlval1]
+        data.ctrl = [0, ctrlval2, ctrlval1]
 
-        if (data.time > 3 and data.time < 4):
-            data.xfrc_applied[1] = [20, 0, 0, 0, 0, 0]
+        if (data.time > 2 and data.time < 5):
+            data.xfrc_applied[2] = [0, 0, 0, 0, 0, 0]
             
         else:
-            data.xfrc_applied[1] = [0, 0, 0, 0, 0, 0]
+            data.xfrc_applied[2] = [0, 0, 0, 0, 0, 0]
         # Step the simulation.
         mujoco.mj_step(model, data)
 
@@ -97,16 +105,42 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         # print(cur_pos1)
         # print(des_qpos1)
         # print(ctrlval1)
-        print((data.qpos.copy())[plant_idx])
-        print("-------------------------------------")
-        print(cur_pos2)
-        print(des_qpos2)
+        # print((data.qpos.copy())[plant_idx])
+        print(cur_pos2 * 180 / np.pi)
+        print(err2)
         print(ctrlval2)
+        print("-------------------------------------")
+
+        
+        viewer.user_scn.ngeom = 0
+        mujoco.mjv_initGeom(
+            viewer.user_scn.geoms[0],
+            type=mujoco.mjtGeom.mjGEOM_SPHERE,
+            size=[0.03, 0.03, 0],
+            pos=(mat2 @ proj_vec2),
+            mat=np.eye(3).flatten(),
+            rgba=np.array([1, 0, 0, 1])
+        )
+        viewer.user_scn.ngeom += 1
+        mujoco.mjv_initGeom(
+            viewer.user_scn.geoms[1],
+            type=mujoco.mjtGeom.mjGEOM_SPHERE,
+            size=[0.03, 0.03, 0],
+            pos = [0, 0, 0],
+            # pos=com,
+            mat=np.eye(3).flatten(),
+            rgba=np.array([1, 0, 0, 1])
+        )
+        viewer.user_scn.ngeom += 1
 
         if len(frames) < data.time * FRAMERATE:
             renderer.update_scene(data)
             pixels = renderer.render()
             frames.append(pixels)
         viewer.sync()
+
+        with viewer.lock():
+            model.vis.scale.com = 0.1
+            # model.vis.scale.frames = 0.1
 
 media.show_video(frames, fps=FRAMERATE)
